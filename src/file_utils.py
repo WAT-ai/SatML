e551087cd94b7058e9caebad6b1208178fcf8fe7
Prahar Ijner <praharijner1997@gmail.com>
2024-09-28 19:34:43 -0400

    Add functions to read dataset metadata

--- src/file_utils.py
+++ src/file_utils.py
@@ -1,7 +1,9 @@
 import os
 import logging
+import pandas as pd
 
 from pathlib import Path
+from typing import Optional
 
 
 def check_file_exist(file_path: str | os.PathLike) -> bool:
@@ -67,3 +69,48 @@ def get_available_datasets(raw_data_dir: str | os.PathLike) -> list[str]:
             available_datasets.append(dataset_name)
 
     return available_datasets
+
+
+def get_dataset_path(raw_data_dir: str | os.PathLike, dataset_name: str) -> str:
+    """helper function to get path to the dataset directory in the raw_data_dir
+
+    Args:
+        raw_data_dir (str | os.PathLike): path to the raw data directory
+        dataset_name (str): name of dataset split
+
+    Returns:
+        str: path to the dataest directory
+    """
+    return Path(raw_data_dir).joinpath(f'STARCOP_{dataset_name}').as_posix()
+
+
+def read_dataset_csv(
+    raw_data_dir: Optional[str | os.PathLike] = None,
+    dataset_name: Optional[str] = None,
+    csv_file_path: Optional[str | os.PathLike] = None,
+) -> pd.DataFrame:
+    """read csv file containing dataset split metadata to a DataFrame
+
+    Args:
+        raw_data_dir (Optional[str  |  os.PathLike]): path to the raw data directory. Can be omitted if csv_file_path is given
+        dataset_name (Optional[str]): name of dataset split. Can be omitted if csv_file_path is given
+        csv_file_path (Optional[str  |  os.PathLike]): path to csv file. Not required if raw_data_dir and dataset_name are given
+
+    Raises:
+        FileNotFoundError: raised if csv file cannot be found
+
+    Returns:
+        pd.DataFrame: dataframe created from the csv file
+    """
+    if not csv_file_path:
+        if not (raw_data_dir and dataset_name):
+            raise ValueError(
+                "dataset_name and raw_data_dir must be specified if csv_file_path is not given")
+
+        dataset_path = get_dataset_path(raw_data_dir, dataset_name)
+        csv_file_path = Path(dataset_path).joinpath(f'{dataset_name}.csv')
+
+    if check_file_exist(csv_file_path):
+        return pd.read_csv(csv_file_path)
+
+    raise FileNotFoundError(f"Couldn't find csv file for {dataset_name}")
