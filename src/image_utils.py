63f407a63909e9325c4b6b867649b2592b1e321f
Harsh <harshpatel1632@gmail.com>
2024-10-26 17:29:33 -0400

    Add check for valid dir

--- src/image_utils.py
+++ src/image_utils.py
@@ -15,7 +15,7 @@ def read_tiff_from_file(file_path: str | os.PathLike) -> np.ndarray:
     Returns:
         np.ndarray: numpy array containing file contents. Assumes BGR format
     """
-    pass # TODO: Finsih this function
+    pass # TODO: Finish this function
 
 
 def plot_tiff_images(dir: str | os.PathLike) -> None:
@@ -29,34 +29,36 @@ def plot_tiff_images(dir: str | os.PathLike) -> None:
         None
     """
 
-    path = f"data/raw_data/STARCOP_train_easy/{dir}" # NOTE: Modularize to work with other datasets as needed
+    dir_path = f"data/raw_data/STARCOP_train_easy/{dir}" # NOTE: Modularize to work with other datasets as needed
     files_to_exclude = ['label_rgba.tif', 'labelbinary.tif', 'mag1c.tif', 'weight_mag1c.tif']
 
-    total_files = 0
-    images = []
+    if os.path.isdir(dir_path): # Ensure the directory exists
+        total_files = 0
+        images = []
+        for file in os.listdir(dir_path):
+            if file in files_to_exclude: # Skip files to exclude
+                continue
 
-    for file in os.listdir(path):
-        if file in files_to_exclude: # Skip files to exclude
-            continue
+            total_files += 1
 
-        total_files += 1
+            file_path = os.path.join(dir_path, file)
+            try:
+                img = Image.open(file_path)
+                images.append((file, img)) # Store both filename and img object
+            except Exception as e:
+                print(f"Error reading {file_path}: {e}")
 
-        file_path = os.path.join(path, file)
-        try:
-            img = Image.open(file_path)
-            images.append((file, img)) # Store both filename and img object
-        except Exception as e:
-            print(f"Error reading {file_path}: {e}")
+        print(f"Extracted {len(images)}/{total_files} images from {dir} directory.")
 
+        def show_image(idx):
+            plt.figure(figsize=(5, 5))
+            plt.imshow(images[idx][1], cmap='gray')
+            plt.title(f"Image {idx + 1} of {len(images)}: {images[idx][0]}")
+            plt.axis('off')
+            plt.show()
 
-    print(f"Extracted {len(images)}/{total_files} images from {dir} directory.")
+        # Create a slider for displaying images
+        interact(show_image, idx=(0, len(images) - 1))
 
-    def show_image(idx):
-        plt.figure(figsize=(5, 5))
-        plt.imshow(images[idx][1], cmap='gray')
-        plt.title(f"Image {idx + 1} of {len(images)}: {images[idx][0]}")
-        plt.axis('off')
-        plt.show()
-
-    # Create a slider for displaying images
-    interact(show_image, idx=(0, len(images) - 1))
+    else:
+        print("Unable to access the provided directory.")
