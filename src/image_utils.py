b684660ed69ec4954f5ea83a54e9c3d536cabbb1
Harsh <harshpatel1632@gmail.com>
2024-10-26 16:37:13 -0400

    Add image slider view function

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,6 +1,8 @@
 import os
-import cv2
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+from ipywidgets import interact
 
 
 def read_tiff_from_file(file_path: str | os.PathLike) -> np.ndarray:
@@ -13,4 +15,48 @@ def read_tiff_from_file(file_path: str | os.PathLike) -> np.ndarray:
     Returns:
         np.ndarray: numpy array containing file contents. Assumes BGR format
     """
-    return cv2.imread(file_path)
+    pass # TODO: Finsih this function
+
+
+def plot_tiff_images(dir: str | os.PathLike) -> None:
+    """
+    Display all .tif images in a directory using a slider
+
+    Args:
+        dir (str | os.PathLike)
+
+    Returns:
+        None
+    """
+
+    path = f"data/raw_data/STARCOP_train_easy/{dir}" # NOTE: Modularize to work with other datasets as needed
+    files_to_exclude = ['label_rgba.tif', 'labelbinary.tif', 'mag1c.tif', 'weight_mag1c.tif']
+
+    total_files = 0
+    images = []
+
+    for file in os.listdir(path):
+        if file in files_to_exclude: # Skip files to exclude
+            continue
+
+        total_files += 1
+
+        file_path = os.path.join(path, file)
+        try:
+            img = Image.open(file_path)
+            images.append((file, img)) # Store both filename and img object
+        except Exception as e:
+            print(f"Error reading {file_path}: {e}")
+
+
+    print(f"Extracted {len(images)}/{total_files} images from {dir} directory.")
+
+    def show_image(idx):
+        plt.figure(figsize=(5, 5))
+        plt.imshow(images[idx][1], cmap='gray')
+        plt.title(f"Image {idx + 1} of {len(images)}: {images[idx][0]}")
+        plt.axis('off')
+        plt.show()
+
+    # Create a slider for displaying images
+    interact(show_image, idx=(0, len(images) - 1))
