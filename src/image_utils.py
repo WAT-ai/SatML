7d239432b58901fd05f38bd357c065bda8b89424
Prahar Ijner <praharijner1997@gmail.com>
2024-11-18 17:31:02 -0500

    Optimize varon iteration

--- src/image_utils.py
+++ src/image_utils.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 from PIL import Image
+from typing import Optional
 
 def read_tiff_from_file(file_path: str | os.PathLike) -> np.ndarray:
     """
@@ -42,11 +43,23 @@ def remove_outliers_with_zscore(data, threshold):
     z_scores = (flat_data - mean) / std_dev
     return flat_data[np.abs(z_scores) < threshold]
 
-"""consumes a path to a directory (easy training), and name of the output file. For each
-folder of images, it computes the varon ratio between each image creating
-a c x c matrix where c is the number of hyperspectral images. Returns total mean and
-standard deviation"""
-def varon_iteration(dir_path, output_file, c_threshold, num_folders):
+def varon_iteration(dir_path: str, output_file: str, c_threshold: float, num_folders: Optional[int]=None):
+    """
+    consumes a path to a directory (easy training), and name of the output file. For each
+    folder of images, it computes the varon ratio between each image creating
+    a c x c matrix where c is the number of hyperspectral images. Returns computed matrix
+
+
+    Args:
+        dir_path (str): path to directory with images
+        output_file (str): path to output file for computed matrix
+        c_threshold (float): z-threshold for outlier filtering
+        num_folders (Optional[int], optional): number of folders to process. Defaults to None.
+        If None, all folders will be processed
+
+    Returns:
+        np.ndarray: compute matrix containing varon ratios for all frequency channels
+    """
     final_matrix = []
     
     image_file_names = [
@@ -67,7 +80,7 @@ def varon_iteration(dir_path, output_file, c_threshold, num_folders):
         "TOA_WV3_SWIR7.tif",
         "TOA_WV3_SWIR8.tif"]
 
-    num_images = 3
+    num_images = len(image_file_names)
 
     all_folders = os.listdir(dir_path)
     
@@ -81,26 +94,28 @@ def varon_iteration(dir_path, output_file, c_threshold, num_folders):
             continue
 
         hyperspectral_images = []
+        image_prime_sums = []
         for i in range(num_images):
             img_path = os.path.join(folder_path, image_file_names[i])
             
             with Image.open(img_path) as img_data:
                 img_array = np.array(img_data)
-                hyperspectral_images.append(img_array) 
+                hyperspectral_images.append(img_array)
+                image_prime_sums.append(np.sum(remove_outliers_with_zscore(img_array, c_threshold)))
 
         
         current_matrix = np.zeros((num_images, num_images)) 
 
         for k in range(num_images):
+            S = hyperspectral_images[k]
+            S_prime_sum = image_prime_sums[k]
             for j in range(k, num_images): 
                 # I didn't do anything with the fact that S should be signal band and B should be background band
-                S = hyperspectral_images[k]
                 B = hyperspectral_images[j]
 
                 # calculate c: note! S/B_prime are 1D arrays
-                S_prime = np.array(remove_outliers_with_zscore(S, c_threshold))
-                B_prime = np.array(remove_outliers_with_zscore(B, c_threshold))
-                c = S_prime.sum()/B_prime.sum()
+                B_prime_sum = image_prime_sums[j]
+                c = S_prime_sum / B_prime_sum
 
                 mean, _ = varonRatio(S, B, c)
                 current_matrix[k, j] = mean
@@ -109,10 +124,8 @@ def varon_iteration(dir_path, output_file, c_threshold, num_folders):
         final_matrix.append(current_matrix)
 
     np.save(output_file, final_matrix)
-    overall_mean = np.mean(final_matrix)
-    overall_stddev = np.std(final_matrix)
 
-    return overall_mean, overall_stddev
+    return final_matrix
     
     
   
