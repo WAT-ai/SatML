Add dice loss and fix normalization

models/UNetModule.py
@@ -38,6 +38,15 @@ class UNet():
         self.input_channels = input_channels
         self.num_classes = num_classes
         self.model = self.unet_model()
+        # temporary values for normalization
+        self.image_mean = tf.constant([0.10667099, 13.674022, 14.076011, 14.384233,
+                                       0.11666037, 1.171873, 0.85495037, 0.4318816,
+                                       0.48891786, 0.11440071, 0.11879135, 0.12649247,
+                                       0.11130015, 0.10874157, 0.10803088, 0.10755966])
+        self.image_std = tf.constant([0.09226333, 13.874699, 15.393596, 17.116693,
+                                      0.17859535, 1.4490424, 0.83888125, 0.52144384,
+                                      0.6939981, 0.16807413, 0.1728661, 0.17979662,
+                                      0.1645278, 0.15060072, 0.138635, 0.12866527])
     
     def load_image(self, datapoint):
         # resize images to 128 x 128 pixels
@@ -47,6 +56,10 @@ class UNet():
             (128, 128),
             method = tf.image.ResizeMethod.NEAREST_NEIGHBOR,
         )
+
+        # normalize image
+        input_image -= self.image_mean
+        input_image /= self.image_std
 
         return input_image, input_mask
     
@@ -136,10 +149,30 @@ class UNet():
         x = last(x)
 
         return tf.keras.Model(inputs=inputs, outputs=x)
+    
+    def dice_loss(self, y_true, y_pred):
+        # y_true shape [batch_size,128,128,1] dtype=float32
+        # y_pred shape [batch_size,128,128,num_classes] dtype=float32
+        # y_pred array of logits
+        smooth = tf.constant(1e-17)
 
-    def train(self, train_dataset, val_dataset, epochs=20, batch_size=64, buffer_size=1000, val_subsplits=5):
+        y_true = tf.cast(y_true, tf.int32)
+        y_true = tf.one_hot(y_true, depth=2, axis=-1)
+        y_true = tf.squeeze(y_true)
+        y_pred = tf.math.softmax(y_pred, axis=-1)
+
+        numerator = tf.reduce_sum(y_true * y_pred)
+        denominator = tf.reduce_sum(y_true + y_pred)
+        # numerator = tf.cast(numerator, tf.float32)
+        # denominator=tf.cast(denominator, tf.float32)
+        loss = 1.0 - 2.0 * (numerator + smooth) / (denominator + smooth)
+
+        return loss
+
+    def train(self, train_dataset, val_dataset, epochs=20, batch_size=64, buffer_size=1000, val_subsplits=1, lr=0.001):
        self.model.compile(optimizer='adam',
-                          loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                        #   loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                          loss=self.dice_loss,
                           metrics=['accuracy'])
        
        train_length, val_length = len(train_dataset), len(val_dataset)
@@ -164,35 +197,24 @@ class UNet():
        model_history = self.model.fit(train_batches, 
                                       epochs=epochs,
                                       steps_per_epoch=steps_per_epoch,
-                                      validation_steps=validation_steps,
+                                    #   validation_steps=validation_steps,
                                       validation_data=val_batches)
        
        return model_history
-    
-    def display(self, display_list):
-        plt.figure(figsize=(15, 15))
-
-        title = ['True Mask', 'Predicted Mask']
-
-        for i in range(len(display_list)):
-            plt.subplot(1, len(display_list), i+1)
-            plt.title(title[i])
-            plt.imshow(tf.keras.utils.array_to_img(display_list[i]))
-            plt.axis('off')
-        plt.show()
 
     def create_mask(self, prediction):
         pred_mask = tf.math.softmax(prediction, axis=-1)
         pred_mask = tf.math.argmax(pred_mask, axis=-1)
         pred_mask = pred_mask[..., tf.newaxis]
-        return pred_mask[0]
+        return pred_mask
 
-    def predict(self, test_dataset, batch_size=64, num=1):
+    def predict_image(self, image, mask):
+        prediction = self.model.predict(image)
+        pred_mask = self.create_mask(prediction)
+        return (image, mask, pred_mask)
+
+    def predict_dataset(self, test_dataset, batch_size=8):
         test_images = test_dataset.map(self.load_image, num_parallel_calls=tf.data.AUTOTUNE)
         test_batches = test_images.batch(batch_size)
-
-        for image, mask in test_batches.take(num):
-            prediction = self.model.predict(image)
-            pred_mask = self.create_mask(prediction)
-            self.display([mask[0], pred_mask])
-       
+        for images, masks in test_batches.take(1):
+            return self.predict_image(images, masks)

models/train_segmentation.py
@@ -4,34 +4,30 @@ import numpy as np
 import tensorflow as tf
 from UNetModule import UNet
 
-def train_segmentation(train_x, train_y, test_x, test_y, input_channels, num_classes, epochs):
+def train_segmentation(train_x, train_y, test_x, test_y, input_channels, num_classes, epochs, output_path):
     """Script to train U-Net segmentation model given numpy arrays containing train and test images and labels.
 
     Images are in n, h, w, c format (float32).
     Labels are in h, w format (uint8).
     """
-    # Normalize images
-    x_max = max(np.max(train_x), np.max(test_x))
-    x_min = min(np.min(train_x), np.min(test_x))
-
-    train_x_norm = (train_x - x_min) / (x_max - x_min)
-    test_x_norm = (test_x - x_min) / (x_max - x_min)
 
     # Correct shape of label arrays (n, h, w) -> (n, h, w, 1)
     train_y_chan = np.expand_dims(train_y, axis=3)
     test_y_chan = np.expand_dims(test_y, axis=3)
 
     # Create tensorflow datasets
-    train_dataset = tf.data.Dataset.from_tensor_slices({"image": train_x_norm, "segmentation_mask": train_y_chan})
-    test_dataset = tf.data.Dataset.from_tensor_slices({"image": test_x_norm, "segmentation_mask": test_y_chan})
+    train_dataset = tf.data.Dataset.from_tensor_slices({"image": train_x, "segmentation_mask": train_y_chan})
+    test_dataset = tf.data.Dataset.from_tensor_slices({"image": test_x, "segmentation_mask": test_y_chan})
 
     # Train model
     unet = UNet(input_channels, num_classes)
     unet.train(train_dataset, test_dataset, epochs=epochs)
+    unet.model.save_weights(f'{output_path}/checkpoint.weights.h5')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-data_path', '--data_path', type=str, default='/home/sarahmakki12/SatML/data/np_data')
+    parser.add_argument('-output_path', '--output_path', type=str, default='/home/sarahmakki12/SatML/logs')
     parser.add_argument('-input_channels', '--input_channels', type=int, default=16)
     parser.add_argument('-num_classes', '--num_classes', type=int, default=2)
     parser.add_argument('-epochs', '--epochs', type=int, default=20)
@@ -44,4 +40,4 @@ if __name__ == '__main__':
     test_y = np.load(f'{args.data_path}/test_y.npy')
 
     # Train model
-    train_segmentation(train_x, train_y, test_x, test_y, args.input_channels, args.num_classes, args.epochs)
+    train_segmentation(train_x, train_y, test_x, test_y, args.input_channels, args.num_classes, args.epochs, args.output_path)