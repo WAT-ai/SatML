ad1b2cf8c5328f4e5d757d86703886a80f9ffdd4
Harsh <harshpatel1632@gmail.com>
2025-04-27 17:18:38 -0400

    Clean up UI and components

--- app/src/components/HeroSection.tsx
+++ app/src/components/HeroSection.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import Image from 'next/image';
+import { Globe } from "@/components/magicui/globe";
+import Link from 'next/link';
+import { AuroraText } from "@/components/magicui/aurora-text";
 
 export default function HeroSection() {
   return (
@@ -9,25 +11,21 @@ export default function HeroSection() {
         
         {/* Image on the left */}
         <div className="w-1/2 h-full relative">
-          <Image
-            src="/earth.jpeg"
-            alt="Earth"
-            fill
-            className="object-cover transform"
-          />
+          <Globe />
         </div>
 
         {/* Text on the right */}
-        <div className="w-1/2 flex flex-col justify-center items-start space-y-6 px-8">
-          <h1 className="text-3xl font-normal max-w-4xl leading-tight">
-            AI-powered methane emissions detection via hyper-spectral satellite imagery.
+        <div className="w-1/2 flex flex-col justify-center items-start px-8">
+          <h1 className="text-3xl font-normal max-w-4xl leading-tight text-white pb-2">
+          <AuroraText>AI-powered</AuroraText> methane emissions detection via hyper-spectral satellite imagery.
           </h1>
-          <p className="text-gray-400 text-lg font-extralight italic">
+          <p className="text-gray-400 text-lg font-extralight italic pb-4">
             Analyze. Predict. Prevent.
           </p>
-          <button className="mt-4 px-6 py-2 border border-white text-white rounded-md hover:bg-white hover:text-black transition">
-            Learn More
-          </button>
+
+          <Link href="#" className="mt-4 px-6 py-2 border border-white text-white rounded-md hover:bg-white hover:text-black transition">
+            Learn More  →
+          </Link>
         </div>
 
       </div>
--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-10 py-6 bg-black text-primary">
-      <div className="text-2xl font-bold">SatML</div>
-      <div className="flex space-x-8 text-sm font-light">
+    <nav className="flex justify-between items-center px-10 py-6 bg-black text-primary text-white">
+      <div className="text-4xl font-medium">SatML</div>
+      <div className="flex space-x-8 text-base font-light">
         <Link href="#">Home</Link>
         <Link href="#">About</Link>
         <Link href="#">Project</Link>
